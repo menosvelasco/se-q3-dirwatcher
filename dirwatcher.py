a2749895ebdb59b0d1bcedb482e1f8a3e75c643e
Menos  Velasco jr <menozvelasco1@gmail.com>
2020-10-12 18:23:52 -0700

    created my own branch

--- dirwatcher.py
+++ dirwatcher.py
@@ -3,27 +3,32 @@
 Dirwatcher - A long-running program
 """
 
-__author__ = "???"
+__author__ = "Manuel velasco"
 
 import sys
+import signal
 
 
 def search_for_magic(filename, start_line, magic_string):
+    """"""
     # Your code here
     return
 
 
 def watch_directory(path, magic_string, extension, interval):
+    """"""
     # Your code here
     return
 
 
 def create_parser():
+    """"""
     # Your code here
     return
 
 
 def signal_handler(sig_num, frame):
+    """"""
     # Your code here
     return
 
